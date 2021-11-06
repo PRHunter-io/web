97f6044d63a73a26cc85541565f51132ee33cc91
pshemcio <daglas92@gmail.com>
2021-11-06 19:24:27 +0100

    Add sign-up view to ModalSignin

--- src/components/ModalSignIn/ModalSignIn.js
+++ src/components/ModalSignIn/ModalSignIn.js
@@ -10,8 +10,24 @@ const ModalStyled = styled(Modal)`
   } */
 `;
 
+const ButtonAccount = styled.button`
+  background-color: transparent;
+  padding: 0;
+  border: none;
+  opacity: 0.7;
+  transition: opacity .3s;
+  &:focus {
+    outline: none;
+  }
+  &:focus-visible,
+  &:hover {
+    opacity: 1;
+  }
+`
+
 const ModalSignIn = (props) => {
   const [showPass, setShowPass] = useState(true);
+  const [isRegistering, setIsRegistering] = useState(false);
   const gContext = useContext(GlobalContext);
 
   const handleClose = () => {
@@ -44,10 +60,10 @@ const ModalSignIn = (props) => {
               <div className="pt-10 pb-6 pl-11 pr-12 bg-black-2 h-100 d-flex flex-column dark-mode-texts">
                 <div className="pb-9">
                   <h3 className="font-size-8 text-white line-height-reset pb-4 line-height-1p4">
-                    Welcome Back
+                    {isRegistering ? 'Create a free account today' : 'Welcome Back'}
                   </h3>
                   <p className="mb-0 font-size-4 text-white">
-                    Log in to continue your account and explore new jobs.
+                    {isRegistering ? 'Create your account to continue and explore new jobs.' : 'Log in to continue your account and explore new jobs.'}
                   </p>
                 </div>
                 <div className="border-top border-default-color-2 mt-auto">
@@ -78,7 +94,7 @@ const ModalSignIn = (props) => {
                     >
                       <i className="fab fa-github pos-xs-abs-cl font-size-7 ml-xs-4"></i>{" "}
                       <span className="d-none d-xs-block">
-                        Log in with Github
+                        {isRegistering ? 'Import from Github' : 'Log in with Github'}
                       </span>
                     </a>
                   </div>
@@ -117,7 +133,7 @@ const ModalSignIn = (props) => {
                       />
                       <a
                         href="/#"
-                        className="show-password pos-abs-cr fas mr-6 text-black-2"
+                        className={`show-password pos-abs-cr fas mr-6 text-black-2${showPass ? "" : " show"}`}
                         onClick={(e) => {
                           e.preventDefault();
                           togglePassword();
@@ -127,21 +143,69 @@ const ModalSignIn = (props) => {
                       </a>
                     </div>
                   </div>
+                  {isRegistering ?
+                    <div className="form-group">
+                      <label
+                        htmlFor="password2"
+                        className="font-size-4 text-black-2 font-weight-semibold line-height-reset"
+                      >
+                        Confirm Password
+                      </label>
+                      <div className="position-relative">
+                        <input
+                          type={showPass ? "password" : "text"}
+                          className="form-control"
+                          id="password2"
+                          placeholder="Enter password"
+                        />
+                        <a
+                          href="/#"
+                          className={`show-password pos-abs-cr fas mr-6 text-black-2${showPass ? "" : " show"}`}
+                          onClick={(e) => {
+                            e.preventDefault();
+                            togglePassword();
+                          }}
+                        >
+                          <span className="d-none">none</span>
+                        </a>
+                      </div>
+                    </div> : ''
+                  }
                   <div className="form-group d-flex flex-wrap justify-content-between">
-                    <label
-                      htmlFor="terms-check"
-                      className="gr-check-input d-flex  mr-3"
-                    >
-                      <input
-                        className="d-none"
-                        type="checkbox"
-                        id="terms-check"
-                      />
-                      <span className="checkbox mr-5"></span>
-                      <span className="font-size-3 mb-0 line-height-reset mb-1 d-block">
-                        Remember password
-                      </span>
-                    </label>
+                    {isRegistering ?
+                      <label
+                        htmlFor="terms-check"
+                        className="gr-check-input d-flex  mr-3"
+                      >
+                        <input
+                          className="d-none"
+                          type="checkbox"
+                          id="terms-check"
+                        />
+                        <span className="checkbox mr-5"></span>
+                        <span className="font-size-3 mb-0 line-height-reset d-block">
+                          Agree to the{" "}
+                          <a href="/#" className="text-primary">
+                            Terms &amp; Conditions
+                          </a>
+                        </span>
+                      </label> :
+                      <label
+                        htmlFor="remember-password"
+                        className="gr-check-input d-flex  mr-3"
+                      >
+                        <input
+                          className="d-none"
+                          type="checkbox"
+                          id="remember-password"
+                        />
+                        <span className="checkbox mr-5"></span>
+
+                        <span className="font-size-3 mb-0 line-height-reset mb-1 d-block">
+                          Remember password
+                        </span>
+                      </label>
+                    }
                     <a
                       href="/#"
                       className="font-size-3 text-dodger line-height-reset"
@@ -151,14 +215,18 @@ const ModalSignIn = (props) => {
                   </div>
                   <div className="form-group mb-8">
                     <button className="btn btn-primary btn-medium w-100 rounded-5 text-uppercase">
-                      Log in{" "}
+                      {isRegistering ? 'Sign up' : 'Log in'}
                     </button>
                   </div>
                   <p className="font-size-4 text-center heading-default-color">
-                    Don’t have an account?{" "}
-                    <a href="/#" className="text-primary">
-                      Create a free account
-                    </a>
+                    {isRegistering ? 'Do you have an account? ' : 'Don’t have an account? '}
+                    <ButtonAccount
+                      type="button"
+                      className="text-primary"
+                      onClick={() => setIsRegistering(!isRegistering)}
+                    >
+                      {isRegistering ? 'Log in!' : 'Create a free account'}
+                    </ButtonAccount>
                   </p>
                 </form>
               </div>
