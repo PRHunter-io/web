69dfaed71d13b55a83eda168bb176fadfeda1b54
unknown <daglas92@gmail.com>
2022-02-02 15:29:10 +0100

    Create basic form layout

--- src/components/Dashboard/user-data.js
+++ src/components/Dashboard/user-data.js
@@ -1,50 +1,75 @@
-import { GithubData } from "./github-data"
-import { Web3Data } from "./web3-data"
+import { useState } from 'react';
+import { Box } from '../Core';
+import { GithubData } from './github-data';
+import { Web3Data } from './web3-data';
 
 export const UserData = ({ userData }) => {
-    return (
-        <div className="container">
-            <div className="mb-15 mb-lg-23">
-                <div className="row">
-                    <div className="col-xxxl-9">
-                        <h5 className="font-weight-semibold mb-11">
-                            Account settings
-                        </h5>
-                        <div className="bg-white shadow-8 rounded-4 pl-sm-10 pl-4 pr-sm-11 pr-4 pt-15 pb-13">
-                            <div className="row mb-10">
-                                <UserDataEntry label="Email">
-                                    {userData.email ? userData.email : 'missing'}
-                                </UserDataEntry>
-                                <UserDataEntry label="Name">
-                                    {userData.display_name ? userData.display_name : 'missing'}
-                                </UserDataEntry>
-                            </div>
+	const [isEdited, setIsEdited] = useState(null);
+	const submitForm = e => {
+		e.preventDefault();
+		console.log('form', e);
+	};
 
-                            <h5 className="font-weight-semibold mb-11">
-                                Linked accounts
-                            </h5>
-                            <div className="row mb-10">
-                                <div className="col-lg-12 mb-10">
-                                    <span className="mb-3">Web3 wallet address</span>
-                                    <Web3Data />
-                                </div>
-                                <div className="col-lg-12 mb-10">
-                                    <span className="mb-3">Github</span>
-                                    <GithubData />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
-}
+	return (
+		<div className='container'>
+			<div className='mb-15 mb-lg-23'>
+				<div className='row'>
+					<div className='col-xxxl-9'>
+						<form onSubmit={submitForm}>
+							<Box className='d-flex justify-content-between align-items-center mb-11'>
+								<h5 className='font-weight-semibold mb-0'>Account settings</h5>
+								<button
+									className='btn btn-primary'
+									type='button'
+									onClick={() => {
+										setIsEdited(!isEdited);
+									}}>
+									{isEdited ? 'Editing' : 'Edit'}
+								</button>
+							</Box>
+							<div className='bg-white shadow-8 rounded-4 pl-sm-10 pl-4 pr-sm-11 pr-4 pt-15 pb-13'>
+								<div className='row mb-10'>
+									<UserDataEntry label='Email' name='email' isEdited={isEdited}>
+										{userData.email ? userData.email : 'missing'}
+									</UserDataEntry>
+									<UserDataEntry label='Name'>
+										{userData.display_name ? userData.display_name : 'missing'}
+									</UserDataEntry>
+								</div>
 
+								<h5 className='font-weight-semibold mb-6'>Linked accounts</h5>
+								<div className='row'>
+									<div className='col-xl-6 mb-10'>
+										<span className='d-block mb-1'>Web3 wallet address</span>
+										<Web3Data isEdited={isEdited} />
+									</div>
+									<div className='col-xl-6 mb-10'>
+										<span className='d-block mb-1'>Github</span>
+										<GithubData />
+									</div>
+								</div>
+								{isEdited ? (
+									<button className='btn btn-primary'>Save!</button>
+								) : null}
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-const UserDataEntry = (props) => (
-    <div className="col-lg-6 mb-4">
-        <span className="mb-1">{props.label}</span>
-        <h6 className="font-weight-semibold">{props.children}</h6>
-    </div>
-)
+const UserDataEntry = ({ label, name, isEdited, children }) => (
+	<div className='col-lg-6 mb-4'>
+		<span className='d-block mb-1'>{label}</span>
+		{isEdited ? (
+			<input
+				className='form-control'
+				name={name}
+				defaultValue={children}></input>
+		) : (
+			<h6 className='font-weight-semibold'>{children}</h6>
+		)}
+	</div>
+);
--- src/components/Dashboard/web3-data.js
+++ src/components/Dashboard/web3-data.js
@@ -1,26 +1,30 @@
 // import { Button, Box, Text } from "@chakra-ui/react";
-import { useEthers, useEtherBalance } from "@usedapp/core";
-import { Box, Button, Text } from "../Core";
-import { formatEther } from "@ethersproject/units";
+import { useEthers, useEtherBalance } from '@usedapp/core';
+import { Box, Button, Text } from '../Core';
+import { formatEther } from '@ethersproject/units';
 
+export const Web3Data = ({ isEdited }) => {
+	const { activateBrowserWallet, account } = useEthers();
+	const etherBalance = useEtherBalance(account);
+	console.log(etherBalance);
 
-export const Web3Data = () =>  {
-    const {activateBrowserWallet, account } = useEthers();
-    const etherBalance = useEtherBalance(account);
-    console.log(etherBalance);
-
-    return account ? (
-      <Box>
-        <h6 className="font-weight-semibold">
-        {account}
-          {account &&
-            `${account.slice(0, 6)}...${account.slice(
-              account.length - 4,
-              account.length
-            )}`}
-        </h6>
-      </Box>
-    ) : (
-      <Button onClick={activateBrowserWallet}>Get wallet address from metamask</Button>
-    );
-  }
+	return account ? (
+		<Box>
+			{isEdited ? (
+				<input
+					className='form-control'
+					name='eth_wallet_address'
+					defaultValue={account}
+				/>
+			) : (
+				<h6 className='font-weight-semibold text-break'>
+					{account && `${account.slice(0, 10)}...${account.slice(-6)}`}
+				</h6>
+			)}
+		</Box>
+	) : (
+		<Button onClick={activateBrowserWallet}>
+			Get wallet address from metamask
+		</Button>
+	);
+};
