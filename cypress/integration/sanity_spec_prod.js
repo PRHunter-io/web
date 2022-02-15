5831d15293c3cdac5d523665c30365c039affbc4
lmyslinski <lukasz.myslinski@datarobot.com>
2022-02-15 09:18:00 +0100

    Added pre-commit hook

--- cypress/integration/sanity_spec_prod.js
+++ cypress/integration/sanity_spec_prod.js
@@ -1,7 +1,7 @@
 describe('user login', () => {
-	it('user can login with email', () => {
-		// Start fron the index page
-		cy.visit('/');
+  it('user can login with email', () => {
+    // Start fron the index page
+    cy.visit('/');
 
     // Open login modal
     cy.findByText(/log in/i).click();
@@ -16,18 +16,18 @@ describe('user login', () => {
     // The new url should include "/dashboard"
     cy.url().should('include', '/dashboard');
 
-		// The new page should contain an h3 with "My bounties"
-		cy.get('h3').contains('My bounties');
-	});
+    // The new page should contain an h3 with "My bounties"
+    cy.get('h3').contains('My bounties');
+  });
 
-	it('user can log out successfully', () => {
-		// Logout
-		cy.get('.show-gr-dropdown').click();
-		cy.findByRole('link', {
-			name: /log out/i,
-		}).click();
+  it('user can log out successfully', () => {
+    // Logout
+    cy.get('.show-gr-dropdown').click();
+    cy.findByRole('link', {
+      name: /log out/i,
+    }).click();
 
-		// Check if back on home page
-		cy.get('h1').contains(/Submit pull requests. Get paid in crypto/i);
-	})
+    // Check if back on home page
+    cy.get('h1').contains(/Submit pull requests. Get paid in crypto/i);
+  });
 });
